2640486a8b815ce33b79ad465ddb29c55f0a7027
JONATHAN ATONG <110544038+atongjonathan@users.noreply.github.com>
2023-06-04 17:00:28 +0300

    remove tokens

--- rain-alert/main.py
+++ rain-alert/main.py
@@ -2,10 +2,7 @@
 from twilio.rest import Client
 import os
 
-account_sid = "AC02d3f350641fa7e70a80d2e9e599377b"
-auth_token = "c485520cea0a52a66ddc2b5959ff4948"
-OWM_endpoint = "https://api.openweathermap.org/data/2.5/weather"
-api_key = "8d10ebe81248b8386d7dc8b5b019d6f5"
+
 # parameters = {
 #     "lat": -1.292066,
 #     "lon": 36.821945,
